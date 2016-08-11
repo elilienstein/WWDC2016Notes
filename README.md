5ebed93f332e1473dd1323ba00863b3433be868d
elilienstein <eric.lilienstein@gmail.com>
2016-08-10 17:05:23 -0700

    deleted Design Guidelines page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # WWDC2016Notes
 Table of Contents
 Getting Started with Swift - Session 404
-Swift API Design Guidelines - Session 403
-What's New in Foundation for Swift - Session 207
+
+
--- WWDC2016Notes.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
+++ WWDC2016Notes.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -6,10 +6,7 @@
  
  
  * [Getting Started With Swift](Getting%20Started%20With%20Swift)
- * [Swift API Design Guidelines](Swift%20API%20Design%20Guidelines)
- 
- 
- 
+  
  */
 
 //: [Next](@next)
